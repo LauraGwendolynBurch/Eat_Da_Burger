8a2fc26fa16bd6c9e5a89f1507676929e642dc34
Laura Gwen <lauraburch.lb@gmail.com>
2021-01-11 21:30:58 -0500

    added connection file, seed and schema

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baccon Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
