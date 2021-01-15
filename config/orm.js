3636155535429a684d9ca0e7b7d740fc9247c166
Laura Gwen <lauraburch.lb@gmail.com>
2021-01-15 18:21:54 -0500

    added handlebar un and li

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,30 @@
 var connection = require("./connection")
 
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+// function objToSql(ob) {
+//     var arr = [];
+//     for (var key in ob) {
+//         var value = ob[key];
+//         if (Object.hasOwnProperty.call(ob, key)) {
+//             if (typeof value === "string" && value.indexOf(" ") >= 0) {
+//                 value = "'" + value + "'";
+//             }
+//             arr.push(key + "=" + value);
+//         }
+//     }
+
+//     return arr.toString();
+// }
+
 var orm = {
     all: function (tableName, cb) {
         connection.query(`SELECT * FROM ${tableName}`, function (err, results) {
@@ -30,6 +55,39 @@ var orm = {
             cb(result);
         });
     },
-}
+
+    // update: function (table, objColVals, condition, cb) {
+    //     var queryString = "UPDATE " + table;
+
+    //     queryString += " SET ";
+    //     queryString += objToSql(objColVals);
+    //     queryString += " WHERE ";
+    //     queryString += condition;
+
+    //     console.log(queryString);
+    //     connection.query(queryString, function (err, result) {
+    //         if (err) {
+    //             throw err;
+    //         }
+
+    //         cb(result);
+    //     });
+    // },
+
+    // delete: function (table, condition, cb) {
+    //     var queryString = "DELETE FROM " + table;
+    //     queryString += " WHERE ";
+    //     queryString += condition;
+
+    //     connection.query(queryString, function (err, result) {
+    //         if (err) {
+    //             throw err;
+    //         }
+
+    //         cb(result);
+    //     });
+    // }
+};
+
 
 module.exports = orm
